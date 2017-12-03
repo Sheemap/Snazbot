8ed9bdeff870a0d7064c46631566129cb97706d3
Jamis <jamman98@gmail.com>
2017-12-03 01:54:48 -0700

    Messages can now auto delete after set time period. Keeps channel clean.

--- includes/common.js
+++ includes/common.js
@@ -1,12 +1,26 @@
 'use strict'
+const sleep = require('sleep-promise');
+
+const logger = require('./logger.js');
+
+var msgs = exports.msgs = [];
 
 exports.sendMsg = function(msg,content,reply,timeout){
+    let sent;
     if(reply){
-		msg.reply(content)
+		msg.reply(content).then(message => cb(message));
     }else{
-		msg.channel.send(content)
+		msg.channel.send(content).then(message => cb(message));
     }
     if(!isNaN(timeout)){
-	
+        sleep(timeout*1000).then(function(){
+            sent.delete().catch(function(){logger.log('warn','Tried to delete a nonexistent message!')});
+            msg.delete().catch(function(){logger.log('warn','Tried to delete a nonexistent message!')});
+        })
+    }
+    function cb(message){
+    	sent = message;
+    	msgs.push(message);
+    	msgs.push(msg);
     }
 }
