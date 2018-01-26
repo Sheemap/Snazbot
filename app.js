1b1aa590bc94d3d9127861bba69b7f124daf762f
Jamis <jamman98@gmail.com>
2018-01-25 23:32:13 -0700

    Adding safe haven

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ client.on('ready', () => {
 
 
 client.on('message', msg => {
-    if(exports.christ && msg.guild.id == '104981147770990592' && msg.author.id != '208310407201423371'){
+    if(exports.christ && msg.guild.id == '104981147770990592' && msg.author.id != '208310407201423371' && msg.channel.id != '401989433811009536'){
         filter.filter(msg);
     }
     if(msg.content.startsWith(prefix)){
