5b41a252c080a7130c7d653fcf06aa4ec293bb0a
Sheemap <jamman98@gmail.com>
2019-01-29 18:06:27 -0700

    Most of match processing done

--- includes/db.js
+++ includes/db.js
@@ -19,6 +19,7 @@ exports.createNew = function(name,callback){
         db.run("CREATE TABLE data (disNAM TEXT, disID TEXT, data TEXT)");
         db.run("CREATE TABLE chungus (disNAM TEXT, disID TEXT, lastclaim TEXT, points NUMERIC, lastchungus TEXT)");
         db.run("CREATE TABLE league (disNAM TEXT, disID TEXT, timestamp NUMERIC, summoner TEXT, data TEXT)")
+        db.run("CREATE TABLE league_matches (gameID TEXT, timestamp NUMERIC, season NUMERIC, data TEXT)")
 
         var seconds = new Date() / 1000;
         db.run(`INSERT INTO chungus VALUES ("chungus","000","${seconds}","0","0")`);
